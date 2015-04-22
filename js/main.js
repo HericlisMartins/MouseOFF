38174e67ab9b4a8c4e85f3efbc787d0e952662e7
Jeferson Diniz <silvajeferson@hotmail.com>
2015-04-21 21:58:26 -0300

    #8 Barra de Rolagem Adicionada
    Outros Plugins foram adicionados, Jquery Easing

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top 
+        }, 1400, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
